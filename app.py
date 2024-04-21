057bb74a8e04990f5c21d6f95918fbf11b09131a
CSRept1l1an <csyusifovtelman@gmail.com>
2024-04-22 00:16:43 +0400

    TT

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def homepage():
-    return render_template('HomePage.html')
+# app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run()
+# @app.route('/')
+# def homepage():
+#     return render_template('HomePage.html')
+
+
+
+# if __name__ == '__main__':
+#     app.run()
