bfc1298e01aceba8f0e235fd41d94cf7013bd8af
Telman Yusifov <csyusifovtelman@gmail.com>
2024-05-21 19:50:41 +0400

    First Step

--- app.py
+++ app.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('HomePage.html')
+    return render_template('home.html')
+
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+
+@app.route('/shopping')
+def index():
+    return render_template('shopping.html')
+
+
+@app.route('/about')
+def about_us():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
