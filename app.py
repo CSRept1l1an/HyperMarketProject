90e186d4d9d444e8f75c296bd123366b2fe85576
Telman Yusifov <csyusifovtelman@gmail.com>
2024-05-23 14:29:43 +0400

    First Step

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 product_data = {
     1: {'name': 'Product 1', 'description': 'Brief description of Product 1.', 'price': 19.99},
     2: {'name': 'Product 2', 'description': 'Brief description of Product 2.', 'price': 29.99},
     3: {'name': 'Product 3', 'description': 'Brief description of Product 3.', 'price': 39.99},
     4: {'name': 'Product 4', 'description': 'Brief description of Product 4.', 'price': 49.99},
+    5: {'name': 'Product 5', 'description': 'Brief description of Product 5.', 'price': 59.99},
+    6: {'name': 'Product 6', 'description': 'Brief description of Product 6.', 'price': 69.99},
 }
+
+shopping_list = {}
 
 
 @app.route('/')
@@ -20,18 +25,26 @@ def products():
     return render_template('products.html', products=product_data)
 
 
-@app.route('/product/<int:product_id>')
+# noinspection PyShadowingNames
+@app.route('/product/<int:product_id>', methods=['GET', 'POST'])
 def product(product_id):
     product = product_data.get(product_id)
-    if product:
-        return render_template('product.html', product=product)
-    else:
+    if not product:
         return "Product not found", 404
+
+    if request.method == 'POST':
+        if 'shopping_list' not in session:
+            session['shopping_list'] = []
+        session['shopping_list'].append(product)
+        return redirect(url_for('shopping_list'))
+
+    return render_template('product.html', product=product)
 
 
 @app.route('/shopping')
-def shopping():
-    return render_template('shopping.html')
+def shopping_list():
+    shopping_list = session.get('shopping_list', [])
+    return render_template('shopping.html', shopping_list=shopping_list)
 
 
 @app.route('/about')
@@ -40,4 +53,4 @@ def about_us():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
