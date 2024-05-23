312b0da2483eb616cc52c86f708d4dce51f238f2
Telman Yusifov <csyusifovtelman@gmail.com>
2024-05-23 14:15:40 +0400

    First Step

--- app.py
+++ app.py
@@ -2,19 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+product_data = {
+    1: {'name': 'Product 1', 'description': 'Brief description of Product 1.', 'price': 19.99},
+    2: {'name': 'Product 2', 'description': 'Brief description of Product 2.', 'price': 29.99},
+    3: {'name': 'Product 3', 'description': 'Brief description of Product 3.', 'price': 39.99},
+    4: {'name': 'Product 4', 'description': 'Brief description of Product 4.', 'price': 49.99},
+}
+
 
 @app.route('/')
 def homepage():
     return render_template('home.html')
 
 
-@app.route('/product')
-def product():
-    return render_template('product.html')
+@app.route('/products')
+def products():
+    return render_template('products.html', products=product_data)
+
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    product = product_data.get(product_id)
+    if product:
+        return render_template('product.html', product=product)
+    else:
+        return "Product not found", 404
 
 
 @app.route('/shopping')
-def index():
+def shopping():
     return render_template('shopping.html')
 
 
